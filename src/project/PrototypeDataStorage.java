ff8634437563d0ca0b8f8a02822aed1767b94615
monito444 <advrobles@gmail.com>
2025-02-11 16:19:19 -0500

    Updated PrototypeDataStorage comments

--- src/project/PrototypeDataStorage.java
+++ src/project/PrototypeDataStorage.java
@@ -1,10 +1,10 @@
 public class PrototypeDataStorage{
 	public void prototype(DataStorage storage) {
-		// Read in the input source file 
-		// and output source file the user specified.
-		// Then send the input to the compute engine
+		// Read in the input source file provided by ComputeEngine
+		// if input file does not exist, send a fail status to ComputeEngine
+		// else provide a success status
 		
 		// Read in the output the compute engine provided
-		// Output the result to the output source file
+		// Write the output of the result to the output source file
 	}
 }
