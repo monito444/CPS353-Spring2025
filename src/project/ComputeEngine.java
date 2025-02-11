d679023853ddc51dc47a0a24aac6286bade37156
monito444 <advrobles@gmail.com>
2025-02-11 15:56:22 -0500

    Added the ComputeEngine interface and prototype
    Only has comments for now.

---
+++ src/project/ComputeEngine.java
@@ -0,0 +1,3 @@
+public interface ComputeEngine{
+	// nothing here yet
+}
---
+++ src/project/PrototypeComputeEngine.java
@@ -0,0 +1,15 @@
+public class PrototypeComputeEngine{
+	public void prototype(ComputeEngine engine) {
+		// Reading and writing component
+		// First initialize
+		// From the user specified file input, read in the input from the file
+		
+		// Computation component
+		// With the integer input, calculate the fibonacci sequence
+		// to that given integer.
+		// Then find all the even numbers and compute the sum.
+		
+		// Have the final output be available for the
+		// data storage system.
+	}
+}
