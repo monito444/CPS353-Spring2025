52125348295c3a2442696e6b49a345d551de7689
Adrian <advrobles@gmail.com>
2025-02-12 12:14:16 -0500

    Added the user Interface and prototype (#13)
    Started with comments only

---
+++ src/project/PrototypeUser.java
@@ -0,0 +1,11 @@
+public class PrototypeUser{
+	public void prototype(User user) {
+		// ask the user to input a file name
+		// if file name does not exist, ask the user again to input a file name
+		
+		// ask the user to give optional default delimiters to the output
+		
+		// ask the user where to give a name for the output file
+		// if file name already exists, then overwrite the file.
+	}
+}
