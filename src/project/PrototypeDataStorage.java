f34f3e26094a3b622eeaeb39b5c0da09f4adc0d3
Adrian <advrobles@gmail.com>
2025-02-12 12:11:16 -0500

    Added the DataStorage interface and prototype (#14)
    * Added the DataStorage interface and prototype
    
    Just with comments
    
    * Updated PrototypeDataStorage comments

---
+++ src/project/PrototypeDataStorage.java
@@ -0,0 +1,10 @@
+public class PrototypeDataStorage{
+	public void prototype(DataStorage storage) {
+		// Read in the input source file provided by ComputeEngine
+		// if input file does not exist, send a fail status to ComputeEngine
+		// else provide a success status
+		
+		// Read in the output the compute engine provided
+		// Write the output of the result to the output source file
+	}
+}
