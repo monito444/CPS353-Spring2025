4809f12d2ba3ca3ac39e458d9de390bc2ce85694
tlonglol <ltyler446@gmail.com>
2025-02-19 14:41:50 -0500

    Added Smoke test and implementation for compute engine (#32)

---
+++ src/project/ComputeEngineImp.java
@@ -0,0 +1,8 @@
+package src.project;;
+
+public class ComputeEngineImp {
+    private DataStorage dataStorageAPI;
+    private User userAPI;
+
+
+}
---
+++ test/project/TestComputeEngine.java
@@ -0,0 +1,17 @@
+package project;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+import org.mockito.Mockito;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
+
+import src.project.DataStorage;
+import src.project.User;
+
+public class TestComputeEngine {
+    DataStorage mockDataStorage = Mockito.mock(DataStorage.class);
+    User mockUser = Mockito.mock(User.class);
+
+}
