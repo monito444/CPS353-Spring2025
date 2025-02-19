6c50df25abc85eb480071928afaf9f8b1a798afe
azip3 <30534318+azip3@users.noreply.github.com>
2025-02-19 14:55:08 -0500

    Update to import DataStorage

--- test/InMemoryDataStorageAPI.java
+++ test/InMemoryDataStorageAPI.java
@@ -1,6 +1,7 @@
 import java.util.List;
+import src.project.DataStorage;
 
-public class InMemoryDataStoreAPI implements DataStoreAPI {
+public class InMemoryDataStoreAPI implements DataStore {
     private InMemoryInputImp;
     private InMemoryOutputImp ;
 
