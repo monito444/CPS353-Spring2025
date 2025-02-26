9c5e646bac280b7bd2ec5efc0ac61361f14c3986
azip3 <30534318+azip3@users.noreply.github.com>
2025-02-26 14:49:52 -0500

    Added Coordination Component.
    Added Coordination Component Ass. 4 part 2.

--- src/project/UserImp.java
+++ src/project/UserImp.java
@@ -10,8 +10,31 @@ public class UserImp implements User{
 	}
 	
 	
+	
 	@Override
 	public ComputeResult compute(ComputeRequest request) {
+		if (request==null){
+			throw new IllegalArgumentException("Computation Cannot Take Null");
+		}
+		
+		UserInputConfig inputConfiguration= request.getInputConfig();
+		DataStorageReadResult readResult =dataStorageAPI.read(inputConfiguration);		
+		Iterable<Integer> integer=readResult.getResults();
+		
+		if (integer!=null) {
+			return ComputeResult.SUCCESS;
+		}
+		
+		for (Integer i: integer) {//sends to compute Engine.
+			//assuming all 'i' will be integers obviously.
+			computeEngineAPI.compute(i);
+		}
+		
+		UserOutputConfig outputConfig=request.getOutputConfig();
 		return null;
+		
+		
 	}
+	
+	
 }
