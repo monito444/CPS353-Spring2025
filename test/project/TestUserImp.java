13369d20cdd8fe1f8fdf74f7b3821c9f2ac08c7b
azip3 <30534318+azip3@users.noreply.github.com>
2025-03-06 19:17:16 -0500

    Update TestUserImp.java

--- test/project/TestUserImp.java
+++ test/project/TestUserImp.java
@@ -2,7 +2,6 @@ package project;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Assertions;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
