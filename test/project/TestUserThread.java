13e4ec1eb9ed7c30c1e5a303a0bfce77a158742a
monito444 <advrobles@gmail.com>
2025-04-02 15:14:27 -0400

    Update TestUserThread.java

--- test/project/TestUserThread.java
+++ test/project/TestUserThread.java
@@ -15,7 +15,7 @@ public class TestUserThread {
 
 	public void run(String outputPath) {
 		char delimiter = ';';
-		String inputPath = "test/project" + File.separatorChar + "testInputFile.test";
+		String inputPath = "project" + File.separatorChar + "testInputFile.test";
 		
 		// TODO 4: Call the appropriate method(s) on the coordinator to get it to 
 		// run the compute job specified by inputPath, outputPath, and delimiter
