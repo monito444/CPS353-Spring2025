07a7a9800c2bea09cbd71ba11b24d57c882f1ee3
monito444 <advrobles@gmail.com>
2025-04-02 13:10:49 -0400

    Added new test files for assignment 6

---
+++ test/project/TestUserThread.java
@@ -0,0 +1,24 @@
+package project;
+
+import java.io.File;
+
+
+public class TestUserThread {
+	
+	// TODO 3: change the type of this variable to the name you're using for your
+	// @NetworkAPI interface; also update the parameter passed to the constructor
+	private final ComputationCoordinator coordinator;
+
+	public TestUser(ComputationCoordinator coordinator) {
+		this.coordinator = coordinator;
+	}
+
+	public void run(String outputPath) {
+		char delimiter = ';';
+		String inputPath = "test" + File.separatorChar + "testInputFile.test";
+		
+		// TODO 4: Call the appropriate method(s) on the coordinator to get it to 
+		// run the compute job specified by inputPath, outputPath, and delimiter
+	}
+
+}
