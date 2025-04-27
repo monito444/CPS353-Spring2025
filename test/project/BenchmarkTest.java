3334f65d6f2a8c00e6f176d22f4c21c9cc8ed4a0
tlonglol <ltyler446@gmail.com>
2025-04-26 22:28:25 -0400

    Benchmark (#74)
    * Benchmark unfinished
    
    * Update BenchmarkTest.java
    
    * Update BenchmarkTest.java

---
+++ test/project/BenchmarkTest.java
@@ -0,0 +1,32 @@
+package project;
+
+public class BenchmarkTest {
+
+    public static void main(String[] args) {
+        ComputeEngine original = new ComputeEngineImp();
+        ComputeEngine faster = new ComputeEngineImpImproved();
+
+        int testValue = 20;
+
+        long startOriginal = System.nanoTime();
+        original.compute(testValue);
+        long endOriginal = System.nanoTime();
+        long durationOriginal = endOriginal - startOriginal;
+
+        long startFaster = System.nanoTime();
+        faster.compute(testValue);
+        long endFaster = System.nanoTime();
+        long durationFaster = endFaster - startFaster;
+
+        System.out.println("Original time: " + durationOriginal + " ns");
+        System.out.println("Faster time: " + durationFaster + " ns");
+
+        double improvement = ((double) (durationOriginal - durationFaster) / durationOriginal) * 100;
+
+        if (improvement >= 10.0) {
+            System.out.println("Benchmark passed! Faster version is at least 10% faster.");
+        } else {
+            System.out.println("Benchmark failed. Faster version is not fast enough.");
+        }
+    }
+}
