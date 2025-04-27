08f6726893901b15a43342b6f612d777f6dcf535
tlonglol <ltyler446@gmail.com>
2025-04-26 22:22:47 -0400

    Update BenchmarkTest.java

--- test/project/BenchmarkTest.java
+++ test/project/BenchmarkTest.java
@@ -4,11 +4,7 @@ public class BenchmarkTest {
 
     public static void main(String[] args) {
         ComputeEngine original = new ComputeEngineImp();
-<<<<<<< Updated upstream
-        //ComputeEngine faster = new ComputeEngineImpImproved();
-=======
         ComputeEngine faster = new ComputeEngineImpImproved();
->>>>>>> Stashed changes
 
         int testValue = 20;
 
@@ -18,20 +14,16 @@ public class BenchmarkTest {
         long durationOriginal = endOriginal - startOriginal;
 
         long startFaster = System.nanoTime();
-<<<<<<< Updated upstream
-        //faster.compute(testValue);
-=======
         faster.compute(testValue);
->>>>>>> Stashed changes
         long endFaster = System.nanoTime();
         long durationFaster = endFaster - startFaster;
 
         System.out.println("Original time: " + durationOriginal + " ns");
         System.out.println("Faster time: " + durationFaster + " ns");
 
-        double improvement = ((double)(durationOriginal - durationFaster) / durationOriginal) * 100;
+        double improvement = ((double) (durationOriginal - durationFaster) / durationOriginal) * 100;
 
-        if(improvement >= 10.0) {
+        if (improvement >= 10.0) {
             System.out.println("Benchmark passed! Faster version is at least 10% faster.");
         } else {
             System.out.println("Benchmark failed. Faster version is not fast enough.");
