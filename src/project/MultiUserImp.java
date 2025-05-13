79a24b32947ac0470ae146607a3691daadbad431
monito444 <advrobles@gmail.com>
2025-05-12 22:08:07 -0400

    Set up producer/consumer multithreading in MultiUserImp
    Set the max request queue to 5.  Separated io tasks and cpu tasks.

--- src/project/MultiUserImp.java
+++ src/project/MultiUserImp.java
@@ -2,6 +2,8 @@ package project;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -15,28 +17,67 @@ public class MultiUserImp implements User {
     private final ComputeEngine computeEngineAPI;
     private final DataStorage dataStorageAPI;
     private final ExecutorService executorService;
+    private final ExecutorService cpuService;
+    private final BlockingQueue<ComputeRequest> requestQueue;
+    private static final int MAX_SPACE = 5;
 
     public MultiUserImp(ComputeEngine computeEngineAPI, DataStorage dataStorageAPI) {
         this.computeEngineAPI = computeEngineAPI;
         this.dataStorageAPI = dataStorageAPI;
         this.executorService = Executors.newFixedThreadPool(4);
-        
+        this.cpuService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        this.requestQueue = new ArrayBlockingQueue<>(MAX_SPACE);
+        startUp();
     }
 
     @Override
     public ComputeResult compute(ComputeRequest request) {
-        if (request == null) {
-            System.err.println("Invalid argument: Computation cannot take null");
-            return ComputeResult.FAILURE;
-        }
-
         try {
+        	executorService.submit(() -> {
+        		try {
+        			requestQueue.put(request);
+        		} catch (InterruptedException e) {
+        			Thread.currentThread().interrupt();
+        		}
+        	});
+        	
+        	return ComputeResult.SUCCESS;
+        } catch (Exception e) {
+        	System.err.println("There was an error when submitting task: " + e.getMessage());
+        	return ComputeResult.FAILURE;
+        }
+    }
+    
+    private void startUp() {
+    	for(int i = 0; i < Runtime.getRuntime().availableProcessors(); i++) {
+    		cpuService.submit(() -> {
+    			while(true) {
+    				try {
+    					ComputeRequest request = requestQueue.poll(1, TimeUnit.SECONDS);
+    					if(request != null) {
+    						perform(request);
+    					} else {
+    						System.out.println("No tasks in queue.");
+    					}
+    				} catch (InterruptedException e) {
+    					Thread.currentThread().interrupt();
+    					break;
+    				}
+    			}
+    		});
+    	}
+    }
+    
+    private void perform(ComputeRequest request) {
+    	try {
+        	
+        	
             UserInputConfig inputConfig = request.getInputConfig();
             UserOutputConfig outputConfig = request.getOutputConfig(); 
 
             DataStorageReadResult readResult = dataStorageAPI.read(inputConfig);
             if (readResult == null || readResult.getResults() == null) {
-                return ComputeResult.FAILURE;
+                throw new Exception("Something went wrong while reading the input data");
             }
 
             List<Future<String>> futures = new ArrayList<>();
@@ -48,26 +89,21 @@ public class MultiUserImp implements User {
             for (Future<String> future : futures) {
                 outputData.add(future.get());
             }
-            //outputConfig.writeOutput(outputData);
+
             for(String s : outputData) {
             	WriteResult writeResult = dataStorageAPI.appendSingleResult(outputConfig, s, request.getDelimiter());
             	if(writeResult.getStatus() == WriteResult.WriteResultStatus.FAILURE) {
-            		return ComputeResult.FAILURE;
+            		throw new Exception("Something went wrong while writing output data");
             	}
             }
-
-            return ComputeResult.SUCCESS;
-
-        } catch (InterruptedException | ExecutionException e) {
-            System.err.println("Computation error: " + e.getMessage());
-            return ComputeResult.FAILURE;
         } catch (Exception e) {
             System.err.println("Unexpected error: " + e.getMessage());
-            return ComputeResult.FAILURE;
+            
         }
     }
 
     public void shutdown() {
         executorService.shutdown();
+        cpuService.shutdown();
     }
 }
