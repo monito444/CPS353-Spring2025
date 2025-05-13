4f783f7dac25baa7805ed9c26cb2bf3ac1f5fe6b
Adrian <advrobles@gmail.com>
2025-05-12 22:49:36 -0400

    Set up producer/consumer multithreading in MultiUserImp (#78)
    * Set up producer/consumer multithreading in MultiUserImp
    
    Set the max request queue to 5.  Separated io tasks and cpu tasks.
    
    * Update gradle workflow
    
    * Added synchronized in TestUserThread
    
    * sorted threading

--- test/project/TestUserThread.java
+++ test/project/TestUserThread.java
@@ -14,16 +14,18 @@ public class TestUserThread {
 	}
 
 	public void run(String outputPath) {
-		char delimiter = ';';
-		String inputPath = "test" + File.separatorChar + "project" + File.separatorChar + "testInputFile.test";
-		
-		// TODO 4: Call the appropriate method(s) on the coordinator to get it to 
-		// run the compute job specified by inputPath, outputPath, and delimiter
-		UserInputConfig input = new UserInputConfigImp(inputPath);
-		UserOutputConfig output = new UserOutputConfigImp(outputPath);
-		ComputeRequest request = new ComputeRequest(input, output, delimiter);
-		
-		coordinator.compute(request);
+		synchronized (this) {
+			char delimiter = ';';
+			String inputPath = "test" + File.separatorChar + "project" + File.separatorChar + "testInputFile.test";
+			
+			// TODO 4: Call the appropriate method(s) on the coordinator to get it to 
+			// run the compute job specified by inputPath, outputPath, and delimiter
+			UserInputConfig input = new UserInputConfigImp(inputPath);
+			UserOutputConfig output = new UserOutputConfigImp(outputPath);
+			ComputeRequest request = new ComputeRequest(input, output, delimiter);
+			
+			coordinator.compute(request);
+		}
 	}
 
 }
